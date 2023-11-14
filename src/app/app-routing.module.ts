09454c5ef027b4461464e376d82958c88eb0eb1c
raghul292001 <133097101+raghul292001@users.noreply.github.com>
2023-11-14 18:33:54 +0530

    Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/pages/home/home.component';
+import { CartComponent } from './component/pages/cart/cart.component';
+import { SaleComponent } from './component/pages/sale/sale.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'products',
+    component: HomeComponent
+  },
+  {
+    path:'cart',
+    component:CartComponent
+  },
+  {
+    path:'sale',
+    component:SaleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/pages/home/home.component';
+import { CartComponent } from './component/pages/cart/cart.component';
+import { SaleComponent } from './component/pages/sale/sale.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AdminComponent } from './component/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
